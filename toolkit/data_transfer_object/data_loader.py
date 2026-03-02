3273738bdbf80bc0caed819b84a17a5e79e3be4d
max <sokolovmax@gmail.com>
2026-03-02 18:53:01 +0300

    dataloader: do not check hashes in aitk file

--- toolkit/data_transfer_object/data_loader.py
+++ toolkit/data_transfer_object/data_loader.py
@@ -63,9 +63,10 @@ class FileItemDTO(
         
         use_db_entry = False
         if file_key in size_database:
-            db_entry = size_database[file_key]
-            if db_entry is not None and len(db_entry) >= 3 and db_entry[2] == file_signature:
-                use_db_entry = True
+            # BLIND TRUST MODIFICATION
+            # If the file exists in the JSON, trust the dimensions blindly.
+            # We ignore the hash check (db_entry[2]) completely.
+            use_db_entry = True
         
         if use_db_entry:
             w, h, _ = size_database[file_key]
